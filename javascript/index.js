82dde15ad53e7c306c939d2b3be77476d82d2988
Heber F Barra <heberferreirabarra@gmail.com>
2024-08-30 23:00:12 -0300

    Versão inicial do site

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+"use strict";
+let containersImagens = document.querySelectorAll(".container-imagem:not(:has(a))");
+if (matchMedia("(min-width: 600px)").matches) {
+    containersImagens.forEach((container) => {
+        container.addEventListener("click", () => {
+            if (container.classList.contains("expansao-imagem")) {
+                container.classList.remove("expansao-imagem");
+                container.title = "Clique para expandir";
+                return;
+            }
+            container.classList.add("expansao-imagem");
+            container.title = "Clique para fechar";
+        });
+        container.title = "Clique para expandir";
+    });
+}
---
+++ typescript/index.ts
@@ -0,0 +1,20 @@
+let containersImagens: NodeListOf<HTMLDivElement> = document.querySelectorAll(".container-imagem:not(:has(a))"); 
+
+if (matchMedia("(min-width: 600px)").matches) {
+
+    containersImagens.forEach((container) => {
+        container.addEventListener("click", () => {
+            if (container.classList.contains("expansao-imagem")) {
+                container.classList.remove("expansao-imagem");
+                container.title = "Clique para expandir"
+                return;
+            }
+
+            container.classList.add("expansao-imagem");
+            container.title = "Clique para fechar"
+        })
+
+        container.title = "Clique para expandir"
+    })
+
+}
